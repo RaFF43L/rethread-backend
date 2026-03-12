3c1093aa38fe64ce0f27949b2d8a105af30b13ed
Raffa <raffa@rethread.com>
2026-03-12 12:40:48 -0300

    feat(database): add DB_SYNCHRONIZE env var to force synchronize in production

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,7 +14,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         password: config.getOrThrow<string>('DB_PASSWORD'),
         database: config.getOrThrow<string>('DB_NAME'),
         entities: [__dirname + '/../modules/**/entities/*.entity{.ts,.js}'],
-        synchronize: config.get<string>('NODE_ENV') !== 'production',
+        synchronize:
+          config.get<string>('DB_SYNCHRONIZE') === 'true' ||
+          config.get<string>('NODE_ENV') !== 'production',
         logging: config.get<string>('NODE_ENV') === 'development',
       }),
     }),
