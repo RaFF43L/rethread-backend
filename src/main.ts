a40128cd42ca9ac15b453399d2fda600f0f87aa7
Raffa <raffa@rethread.com>
2026-03-15 22:34:30 -0300

    feat(main): configure CORS settings for specific origin and methods

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ const basicAuth = require('express-basic-auth') as typeof import('express-basic-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+  origin: [/\.segundaaura\.com\.br$/],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+});
 
   app.useGlobalPipes(
     new ValidationPipe({
